a0381a1777f24105aaf80fe254fe8eab69a9837a
Samuele <samueletasciotti@icloud.com>
2025-10-09 17:22:39 +0200

    feat: Create routes for routing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/Home', component: () => import('./components/Home.vue') },
+    { path: '/Login', component: () => import('./components/Login.vue') },
+    { path: '/Deadlines', component: () => import('./components/Deadlines.vue') },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
