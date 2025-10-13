aa142a56b1266092f647d65a60bc8fc7c0dfabed
Samuele <samueletasciotti@icloud.com>
2025-10-13 21:39:46 +0200

    feat: transition for routes(client)

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,62 @@
 <script>
-import Navbar from './components/Navbar.vue';
-import Login from './components/Login.vue';
-import Home from './components/Home.vue';
-import Deadlines from './components/Deadlines.vue';
+import Navbar from "./components/Navbar.vue";
+import Login from "./components/Login.vue";
+import Home from "./components/Home.vue";
+import Deadlines from "./components/Deadlines.vue";
 
 export default {
-      components: {
-        Navbar,
-        Login,
-        Home,
-        Deadlines
-      }
-    }
+  components: {
+    Navbar,
+    Login,
+    Home,
+    Deadlines,
+  },
+};
 </script>
 
-<template>  
+<template>
   <Navbar></Navbar>
-  <router-view></router-view>
+  <transition name="zoom">
+    <router-view> </router-view>
+  </transition>
 </template>
 
+
 <style scoped>
+/* Pausa prima dell'entrata (usiamo delay) */
+.zoom-leave-active,
+.zoom-enter-active {
+  will-change: transform, opacity;
+  backface-visibility: hidden;
+}
+
+.zoom-leave-active {
+  transition: all 0.3s ease;
+}
+
+.zoom-leave-from {
+  transform: scale(1);
+  opacity: 1;
+}
+
+.zoom-leave-to {
+  transform: scale(0.95);
+  opacity: 0;
+}
+
+.zoom-enter-active {
+  transition: all 0.3s ease;
+  transition-delay: 0.15s;
+}
+
+.zoom-enter-from {
+  transform: scale(0.95);
+  opacity: 0;
+}
+
+.zoom-enter-to {
+  transform: scale(1);
+  opacity: 1;
+}
 </style>
+
