c5d5bb0440d950bc10e12360a7115a0f453e5544
Samuele <samueletasciotti@icloud.com>
2025-10-13 21:40:22 +0200

    feat: status for login/logout
    fix: routes for client side

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
 import { createApp } from 'vue'
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import Home from './components/Home.vue'
+import Login from './components/Login.vue'
+import Deadlines from './components/Deadlines.vue'
+import { store } from './store'
 
 const routes = [
-    { path: '/Home', component: () => import('./components/Home.vue') },
-    { path: '/Login', component: () => import('./components/Login.vue') },
-    { path: '/Deadlines', component: () => import('./components/Deadlines.vue') },
+  { path: '/', component: Home },
+  { path: '/login', component: Login },
+  { path: '/deadlines', component: Deadlines },
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 })
+
 const app = createApp(App)
 app.use(router)
+
+// controllo per mantenere in memoria lo stato di login per pulsante logout anche dopo refresh
+store.loggedIn = !!localStorage.getItem('token')
+
 app.mount('#app')
