4c7a820171013ab6ba1050fc3c87b43ef56a69c4
Samuele <samueletasciotti@icloud.com>
2025-10-16 09:30:08 +0200

    fix: logic of logout and storage data of users

--- src/store.js
+++ src/store.js
@@ -1,8 +1,24 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    loggedIn: false,
-    userRole: null, // 'admin' or 'user'
-    userId: null,
-    userName: null,
+  loggedIn: localStorage.getItem("token") ? true : false,
+  userRole: localStorage.getItem("userRole") || null,
+  userId: localStorage.getItem("userId") || null,
+  userName: localStorage.getItem("userName") || null,
 });
+
+// Funzione di logout centralizzata
+export function logout() {
+  // Pulisci localStorage
+  localStorage.removeItem("token");
+  localStorage.removeItem("userRole");
+  localStorage.removeItem("userName");
+  localStorage.removeItem("userId");
+  localStorage.removeItem("userEmail");
+
+  // Resetta lo store
+  store.loggedIn = false;
+  store.userRole = null;
+  store.userName = null;
+  store.userId = null;
+}
